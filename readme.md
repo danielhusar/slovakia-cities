5e03cc2428f3884f7917cdbbef2b2989e3712926
Daniel Husar <dano.husar@gmail.com>
2015-01-10 20:10:58 +0000

    all cities

--- readme.md
+++ readme.md
@@ -1,5 +1,19 @@
-# WIP
+# slovakia-cities [![Build Status](https://secure.travis-ci.org/danielhusar/slovakia-cities.svg?branch=master)](http://travis-ci.org/danielhusar/slovakia-cities)
 
+List all cities in a slovakia.
+
+## Install
+
+```
+npm install --save slovakia-cities
+```
+
+## Example
+
+```javascript
+var cities = require('slovakia-cities')(); //cities divided by regions
+var allCities = require('slovakia-cities').cities(); //array of all citites together
+```
 
 ## License
 
--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 'use strict';
 var assert = require('assert');
-var cities = require('./');
+var cities = require('./')();
+var allCities = require('./').cities();
 
 
-it('it should work', function () {
+it('Regions should work', function () {
 	assert.equal(cities['Bratislavský'][0], 'Bratislava - Staré Mesto');
+	assert.equal(cities['Bratislavský'][cities['Bratislavský'].length - 1], 'Zálesie');
+});
+
+it('All cities should work', function () {
+	assert.equal(allCities[0], 'Bratislava - Staré Mesto');
+	assert.equal(allCities[allCities.length - 1], 'Zemplínsky Branč');
 });
